e79b1a17942f9e959fda937ecdac54ba0986bfb3
Bimba Shrestha <bshrestha.msae@gmail.com>
2022-02-14 21:32:28 -0600

    Fix copy path on windows

--- client/src/blobbackup/scripts/buildwin.ps1
+++ client/src/blobbackup/scripts/buildwin.ps1
@@ -2,7 +2,7 @@ cd restic
 git checkout v0.12.1
 go run build.go
 cd ../
-cp restic/restic src/blobbackup/bin/blobbackup.exe
+cp restic/restic.exe src/blobbackup/bin/blobbackup.exe
 
 src/blobbackup/scripts/generateui.ps1
 pip install .
